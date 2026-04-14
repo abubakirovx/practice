a5c484bb64e5f58bb9c8c8b75f0fcdc537e7e50a
umar <hakimovjahongir585@gmail.com>
2026-04-14 21:28:57 +0900

    feat: python system variables & special methods via dunder

--- run.py
+++ run.py
@@ -1,1 +1,18 @@
 print("Hello World")
+print("Everything is object!")
+
+# Dunder __builtins__, __init__
+
+message = "PYTHON: Everything is object!"
+print(message)
+
+result = type(message)
+print(f"result : {result}")
+
+''' In Python, there are builtin tools:
+ (1) TYPES     > int float str list dict
+ (2) FUNCTIONS > print() len() input() type() str()
+ (3) CONSTANTS > True False None 
+
+'''
+print(dir(__builtins__))
