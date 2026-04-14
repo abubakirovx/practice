d5a609a85644c4350b9b1e4bde439a8ee3219190
umar <hakimovjahongir585@gmail.com>
2026-04-14 21:58:53 +0900

    feat: learn boolean in python

--- primitive.py
+++ primitive.py
@@ -32,5 +32,22 @@ print(f"the result (3) : {result}")
 print(f"original version : {course}")
 
 print("==== boolean =====")
+# functions > type() input() bool() int() str()
+y = input("Give your value for y: ")
+print("y:", y)
 
+result = y.isnumeric()
+print(f"this input 'y' is numeric ? {result}")
 
+# TRUTHY (many) => True, 100, " ", -100, "UMAR", ...
+# FALSY (4)     => False, "", 0, None
+
+test_falsy = ""
+print(f"THE FALSY (1) : {bool(test_falsy)}")
+
+test_falsy = "" or False or None or 0
+print(f"THE FALSY (2) : {bool(test_falsy)}")
+
+# thsi is not FALSY becouse of "Ali" is TRUTHY
+test_falsy = "" or False or None or 0 or "Ali"
+print(f"THE FALSY (3) : {bool(test_falsy)}")
