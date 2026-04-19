1729ba4675760eef6451aa33188aeb187873a7b0
umar <hakimovjahongir585@gmail.com>
2026-04-20 05:32:26 +0900

    feat:train in JavaScricpt

--- train.js
+++ train.js
@@ -1,20 +1,59 @@
 
-function countNumber (word){
-    let countedNumber=0
-    for(let i=0;i < word.length;i++){
+// function countNumber (word){
+//     let countedNumber=0
+//     for(let i=0;i < word.length;i++){
        
-        if (!isNaN(word[i])){
+//         if (!isNaN(word[i])){
             
-            countedNumber++
-        }
+//             countedNumber++
+//         }
+//     }
+//     console.log(`Berilgan tekst ichida ${countedNumber} ta son bor`);
+    
+// }
+// const randomString ="s4ds63ererdf542gfg73=-5h"
+// countNumber(randomString)
+
+
+const animals =['deer','dog','cat','cow','goose','goat','rabbit','chicken','donkey',"mouse","ayiq",'rat']
+
+
+
+
+function findAnimal (txt){
+    let count = 0
+    let foundAnimal=[]
+    for(let i=0; i < animals.length; i++){
+
+        for(let z=0; z < animals[i].length; z++){
+
+
+              for(let q=0; q < txt.length; q++){
+
+                 if(txt.length >= animals[i].length){
+
+                     if(txt[q]===animals[i][z]){
+                        count++
+                   
+                        if(count===animals[i].length){
+                           
+                           
+                          foundAnimal.push(animals[i])
+                          count=0
+                        }
+                    }
+                    
+                 }
+                 
+                }
+                
+            }
+            count=0
     }
-    console.log(`Berilgan tekst ichida ${countedNumber} ta son bor`);
+    return foundAnimal;
     
 }
-const randomString ="s4ds63ererdf542gfg73=-5h"
-countNumber(randomString)
 
-
-
-
+const javob = findAnimal("aotcdg")
+console.log("JAVOB=>",javob);
 
